2e4a3a609efe9549d1889791e033587e57b405e4
lautaroBulla <lautaro.portillo.bulla@gmail.com>
2025-05-11 19:40:55 -0300

    ASD

--- bm-frontend/src/components/GameFinishComponent.vue
+++ bm-frontend/src/components/GameFinishComponent.vue
@@ -8,7 +8,7 @@
     >
         <div class="bg-black w-1/3 border-2 border-gray-500 flex flex-col p-5">
             <p class="flex justify-center">
-                Has finalizado
+                HAS GANADO!!!
             </p>
             <div class="flex justify-center">           
                 <button class="border hover:cursor-pointer" @click="emit('close')">
