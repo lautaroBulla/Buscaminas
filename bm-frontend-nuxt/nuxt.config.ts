3773f0ceeab6220a69c7d5e916a377edabab827c
lautaroBulla <lautaro.portillo.bulla@gmail.com>
2025-05-22 17:30:34 -0300

    front in nuxt

--- bm-frontend-nuxt/nuxt.config.ts
+++ bm-frontend-nuxt/nuxt.config.ts
@@ -30,6 +30,30 @@ export default defineNuxtConfig({
   vite: {
     plugins: [
       tailwindcss()
-    ]
-  }
+    ],
+    server: {
+      watch: {
+        usePolling: true
+      },
+      proxy: {
+        '/api': 
+          { 
+            target: process.env.VITE_URL_BACKEND,
+            changeOrigin: true,
+            rewrite: path => path.replace(/^\/api/, ''),
+            configure: (proxy, _options) => {
+              proxy.on('error', (err, _req, _res) => {
+                console.log('proxy error', err);
+              });
+              proxy.on('proxyReq', (proxyReq, req, _res) => {
+                console.log('Sending Request to the Target:', req.method, req.url);
+              });
+              proxy.on('proxyRes', (proxyRes, req, _res) => {
+                console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+              });
+            }
+          }
+      }
+    }
+  },
 })
--- bm-frontend/src/services/authService.js
+++
@@ -1,7 +0,0 @@
-export const api = {
-    async getUser() {
-        const res = await fetch('api/users');
-        const data = await res.json();        
-        return data;
-    }
-}
