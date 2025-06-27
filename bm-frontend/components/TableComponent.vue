82e72b673d467fe821ac5c0b4d8b237ef94ab5c7
lautaroBulla <lautaro.portillo.bulla@gmail.com>
2025-06-27 14:13:51 -0300

    hemocho pechocho maravichocho

--- bm-frontend/components/TableComponent.vue
+++ bm-frontend/components/TableComponent.vue
@@ -11,7 +11,7 @@
       type: Array,
       required: true
     },
-    modelValue: {
+    page: {
       type: Number,
       required: true
     },
@@ -35,139 +35,145 @@
     return mode === 'dark' ? true : false;
   });
 
-  const emit = defineEmits(['update:modelValue']);
-  const page = ref(props.modelValue);
+  const emit = defineEmits(['changePage', 'lookStats']);
 
   const changePage = (newPage) => {
-    page.value = newPage;
-    emit('update:modelValue', newPage);
+    emit('changePage', newPage);
   }
 </script>
 
 <template>
   <div>
-    <p v-if="games.length === 0" class="text-center">{{ $t('ranking.noGames') }}</p>
 
-    <table v-if="games.length > 0" class="table w-full border-collapse table-fixed">
-      <thead>
-        <tr>
-          <th class="tableHeader">#</th>
-          <th class="tableHeader">{{ $t('ranking.username') }}</th>
-          <th class="tableHeader">{{ $t('finishGame.time') }}</th>
-          <th class="tableHeader md:hidden">{{ $t('ranking.stats') }}</th>
-          <th class="tableHeader hidden md:table-cell">{{ $t('finishGame.3bv') }}</th>
-          <th class="tableHeader hidden md:table-cell">{{ $t('finishGame.clicks') }}</th>
-          <th class="tableHeader hidden md:table-cell">{{ $t('finishGame.efficiency') }}</th>
-          <th class="tableHeader hidden md:table-cell">{{ $t('finishGame.help') }}</th>
-          <th class="tableHeader hidden md:table-cell">{{ $t('ranking.date') }}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(game, index) in games" :key="game.id">
-          <td class="tableCell">{{ index + 1 + ((page-1) * 10)}}</td>
-          <td class="tableCell">{{ game.user.username }}</td>
-          <td class="tableCell">{{ game.seconds }}s</td>
-          <td class="tableCell md:hidden">ver</td>
-          <td class="tableCell hidden md:table-cell">{{ game.n3BV }}</td>
-          <td class="tableCell hidden md:table-cell">{{ game.clicks }}</td>
-          <td class="tableCell hidden md:table-cell">{{ game.efficiency }}%</td>
-          <td class="tableCell hidden md:table-cell">{{ game.help }}</td>
-          <td class="tableCell hidden md:table-cell">{{ new Date(game.createdAt).toLocaleDateString() }}</td>
-        </tr>
-      </tbody>
-    </table>
-    <div v-if="games.length > 0" class="pagination flex flex-row items-center justify-center gap-x-5">
-      <button 
-        class="secondary"
-        :disabled="page === 1 ? true : false"
-        @click="changePage(1)"
-      >
-        <img 
-          :src="isDark ? arrowLeftLDark : arrowLeftLLight"
-          class="w-[12px] h-[12px] md:w-[14px] md:h-[14px]"      
+    <div v-if="games.length > 0" class="table-container">
+      <table class="table w-full border-collapse table-fixed">
+        <thead>
+          <tr>
+            <th class="tableHeader w-1/12">#</th>
+            <th class="tableHeader md:w-2/12">{{ $t('ranking.username') }}</th>
+            <th class="tableHeader md:w-2/12">{{ $t('finishGame.time') }}</th>
+            <th class="tableHeader md:hidden">{{ $t('ranking.stats') }}</th>
+            <th class="tableHeader hidden md:table-cell">{{ $t('finishGame.3bv') }}</th>
+            <th class="tableHeader hidden md:table-cell">{{ $t('finishGame.clicks') }}</th>
+            <th class="tableHeader hidden md:table-cell">{{ $t('finishGame.efficiency') }}</th>
+            <th class="tableHeader hidden md:table-cell">{{ $t('finishGame.help') }}</th>
+            <th class="tableHeader hidden md:table-cell">{{ $t('ranking.date') }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(game, index) in games" :key="game.id">
+            <td class="tableCell w-1/12">{{ index + 1 + ((page-1) * 10)}}</td>
+            <td class="tableCell md:w-2/12">{{ game.user.username }}</td>
+            <td class="tableCell md:w-2/12">{{ game.seconds }}s</td>
+            <td 
+              class="tableCell hover:cursor-pointer hover:opacity-80 md:hidden"
+              @click="emit('lookStats', index)"
+            >
+              {{ $t('ranking.lookStats') }}
+            </td>
+            <td class="tableCell hidden md:table-cell">{{ game.n3BV }}</td>
+            <td class="tableCell hidden md:table-cell">{{ game.clicks }}</td>
+            <td class="tableCell hidden md:table-cell">{{ game.efficiency }}%</td>
+            <td class="tableCell hidden md:table-cell">{{ game.help }}</td>
+            <td class="tableCell hidden md:table-cell">{{ new Date(game.createdAt).toLocaleDateString() }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div v-if="games.length > 1" class="pagination flex flex-row items-center justify-center gap-x-5">
+        <button 
+          class="secondary-sm md:secondary"
+          :disabled="page === 1 ? true : false"
+          @click="changePage(1)"
         >
-      </button>
-      <button 
-        class="secondary" 
-        :disabled="page === 1 ? true : false"
-        @click="changePage(page-1)"
-      >
-        <img 
-          :src="isDark ? arrowLeftDark : arrowLeftLight"
-          class="w-[12px] h-[12px] md:w-[14px] md:h-[14px]"
+          <img 
+            :src="isDark ? arrowLeftLDark : arrowLeftLLight"
+            class="w-[12px] h-[12px] md:w-[14px] md:h-[14px]"      
+          >
+        </button>
+        <button 
+          class="secondary-sm md:secondary" 
+          :disabled="page === 1 ? true : false"
+          @click="changePage(page-1)"
         >
-      </button>
-      <button 
-        v-if="page - 4 > 0 && page === totalPages" 
-        class="buttonPagination"
-        @click="changePage(page-4)"
-      >
-        {{ page - 4 }}
-      </button>
-      <button 
-        v-if="page - 3 > 0 && (page === totalPages || page === totalPages-1)" 
-        class="buttonPagination"
-        @click="changePage(page-3)"
-      >
-        {{ page - 3 }}
-      </button>
-      <button 
-        v-if="page - 2 > 0" 
-        class="buttonPagination"
-        @click="changePage(page-2)"
-      >
-        {{ page - 2 }}
-      </button>
-      <button 
-        v-if="page - 1 > 0" 
-        class="buttonPagination"
-        @click="changePage(page-1)"
-      >
-        {{ page - 1 }}
-      </button>
+          <img 
+            :src="isDark ? arrowLeftDark : arrowLeftLight"
+            class="w-[12px] h-[12px] md:w-[14px] md:h-[14px]"
+          >
+        </button>
+        <button 
+          v-if="page - 4 > 0 && page === totalPages" 
+          class="buttonPagination"
+          @click="changePage(page-4)"
+        >
+          {{ page - 4 }}
+        </button>
+        <button 
+          v-if="page - 3 > 0 && (page === totalPages || page === totalPages-1)" 
+          class="buttonPagination"
+          @click="changePage(page-3)"
+        >
+          {{ page - 3 }}
+        </button>
+        <button 
+          v-if="page - 2 > 0" 
+          class="buttonPagination"
+          @click="changePage(page-2)"
+        >
+          {{ page - 2 }}
+        </button>
+        <button 
+          v-if="page - 1 > 0" 
+          class="buttonPagination"
+          @click="changePage(page-1)"
+        >
+          {{ page - 1 }}
+        </button>
 
-      <span class="underline">
-        {{ page }}
-      </span>
+        <span class="underline">
+          {{ page }}
+        </span>
 
-      <button 
-        v-if="page + 1 <= totalPages" 
-        class="buttonPagination"
-        @click="changePage(page+1)"
-      >
-        {{ page + 1 }}
-      </button>
-      <button 
-        v-if="page + 2 <= totalPages" 
-        class="buttonPagination"
-        @click="changePage(page+2)"
-      >
-        {{ page + 2 }}
-      </button>
-      <button 
-        v-if="page + 3 <= totalPages && (page === 1 || page === 2)" 
-        class="buttonPagination"
-        @click="changePage(page+3)"
-      >
-        {{ page + 3 }}
-      </button>
-      <button 
-        v-if="page + 4 <= totalPages && (page === 1)" 
-        class="buttonPagination"
-        @click="changePage(page+4)"
-      >
-        {{ page + 4 }}
-      </button>
-      <button 
-        class="secondary" 
-        :disabled="page === totalPages ? true : false"
-        @click="changePage(page+1)"
-      >
-        <img 
-          :src="isDark ? arrowRightDark : arrowRightLight"
-          class="w-[12px] h-[12px] md:w-[14px] md:h-[14px]"
+        <button 
+          v-if="page + 1 <= totalPages" 
+          class="buttonPagination"
+          @click="changePage(page+1)"
         >
-      </button>
+          {{ page + 1 }}
+        </button>
+        <button 
+          v-if="page + 2 <= totalPages" 
+          class="buttonPagination"
+          @click="changePage(page+2)"
+        >
+          {{ page + 2 }}
+        </button>
+        <button 
+          v-if="page + 3 <= totalPages && (page === 1 || page === 2)" 
+          class="buttonPagination"
+          @click="changePage(page+3)"
+        >
+          {{ page + 3 }}
+        </button>
+        <button 
+          v-if="page + 4 <= totalPages && (page === 1)" 
+          class="buttonPagination"
+          @click="changePage(page+4)"
+        >
+          {{ page + 4 }}
+        </button>
+        <button 
+          class="secondary-sm md:secondary" 
+          :disabled="page === totalPages ? true : false"
+          @click="changePage(page+1)"
+        >
+          <img 
+            :src="isDark ? arrowRightDark : arrowRightLight"
+            class="w-[12px] h-[12px] md:w-[14px] md:h-[14px]"
+          >
+        </button>
+      </div>
     </div>
+
+    <p v-else class="text-center">{{ $t('ranking.noGames') }}</p>
   </div>
 </template>
--- bm-frontend/pages/ranking.vue
+++ bm-frontend/pages/ranking.vue
@@ -2,8 +2,11 @@
   import { ref, onMounted } from 'vue';
 
   onMounted(async () => {
-    changeDifficulty();
+    await changeDifficulty();
+    await getGame();
   });
+
+  const { isMobile } = useIsMobile();
 
   const { findByDifficulty } = useGame();
   const rows = ref(9);
@@ -13,28 +16,47 @@
   const games = ref([]);
   const totalPages = ref(null);
 
-  const setCustomValues = ({ rows: customRows, cols: customCols, mines: customMines }) => {
+  const setCustomValues = async ({ rows: customRows, cols: customCols, mines: customMines }) => {
     page.value = 1;
+    totalPages.value = null;
     rows.value = customRows;
     cols.value = customCols;
     mines.value = customMines;
-    changeDifficulty();
+    await changeDifficulty();
   }
 
-  watch(page, async(newPage) => {
+  const changePage = async (newPage) => {
     page.value = newPage;
-    changeDifficulty();
-  })
+    await changeDifficulty();
+  }
 
 
   const changeDifficulty = async () => {
     try {
-      const response = await findByDifficulty(rows.value, cols.value, mines.value, page.value, 10);
-      console.log(response);
+      const response = await findByDifficulty(rows.value, cols.value, mines.value, page.value, 10)
       games.value = response.games;
       totalPages.value = response.totalPages;
+      await getGame();
     } catch (error) {
       console.error('Error fetching games:', error);
+    }
+  }
+
+  const indexGame = ref(0);
+  const game = ref(null);
+  const rankingGame = ref(1);
+  const handleLookStats = (index) => {
+    indexGame.value = index;
+    getGame();
+  };
+  const getGame = () => {
+    if (games.value.length > 0) {
+      const globalIndex = (page.value - 1) * 10 + 1;
+      game.value = games.value[indexGame.value];
+      rankingGame.value = globalIndex + indexGame.value;
+    } else {
+      game.value = null;
+      rankingGame.value = null;
     }
   }
 </script>
@@ -42,7 +64,7 @@
 <template>
 
   <div class="w-full flex justify-center">
-    <div class="w-full flex flex-col max-w-5xl p-4 space-y-3">  
+    <div class="w-full flex flex-col max-w-5xl p-4 space-y-5 md:space-y-3">  
 
       <TableHeaderComponent 
         @change="setCustomValues"
@@ -51,11 +73,18 @@
       <div class="border-b-2 border-[#adb5bd]"></div>
 
       <TableComponent 
-        v-model="page"
         :games="games"
+        :page="page"
         :totalPages="totalPages"
+        @changePage="changePage"
+        @lookStats="handleLookStats"
       />
 
+      <StatsComponent
+        v-if="isMobile"
+        :game="game"
+        :rankingGame="rankingGame"
+      />
     </div>
   </div>
 
